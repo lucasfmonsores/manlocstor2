e484de921d5708f4600e955e6bb33fbb67e2a0e7
lucasfmonsores <lucasfmonsores@gmail.com>
2022-08-14 13:23:02 -0300

    funcoes

---
+++ js/script1.js
@@ -0,0 +1,19 @@
+class Produto {
+
+   constructor() {
+      this.id = 0;
+      this.nomeProduto = ''
+      this.valor = 0;
+   }
+   adicionar() {
+      alert('Vamos adicionar um produto');
+   }
+
+   excluir() {
+
+      alert('excluir');
+   }
+}
+
+
+var produto = new Produto()
